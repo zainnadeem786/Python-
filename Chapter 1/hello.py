81b84a688bf405d48e6e29c54e7abb54cbf8dd8a
zainnadeem786 <zainnadeemzainnadeem80@gmail.com>
2024-05-18 09:00:36 -0700

    Python Last week Practice

--- Chapter 1/hello.py
+++ Chapter 1/hello.py
@@ -1,1 +1,17 @@
 print("Hello World")
+# Using Variables in Python
+message = "Hello_World"
+print(message)
+message = "Hello Python Crash Course World"
+print(message)
+# NameError in Python
+message = "Hi Dear"
+print(message)
+# Methods in Python (Changing Case)
+hello = "my name is zain"
+print(hello.title())
+# Using Variables in String
+first_name = "Zain"
+last_name = "Nadeem"
+print(f"{first_name} {last_name}")
+print(f"Hello!{first_name.title()}")
